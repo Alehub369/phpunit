d9a10680d8ac2bbffbdf9dabe4f8acddd049473e
Alehub369 <lievanoalejandro@gmail.com>
2023-11-10 23:15:04 -0500

    Carga de archivos

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -10,4 +9,13 @@ Route::get('/', function () {
 
 Route::get('about', function () {
     return 'hola soy about';
+});
+
+Route::view('profile', 'profile');
+
+
+Route::post('profile', function (Illuminate\Http\Request $request) {
+    $request->file('photo')->store('profiles');
+
+    return redirect('profile');
 });
