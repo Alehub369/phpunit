b8e0a1daccd012ef5f850c734238d1811b08b66a
Alehub369 <lievanoalejandro@gmail.com>
2023-11-10 21:53:04 -0500

    Archivos de la clase

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,8 +14,13 @@ class Post extends Model
         $this->attributes['name'] = strtolower($value);
     }
 
-    public function getSlugAttribute($value)
+    public function getSlugAttribute()
     {
         return str_replace(' ', '-', $this->name);
+    }
+
+    public function href()
+    {
+        return "blog/{$this->slug}";
     }
 }
